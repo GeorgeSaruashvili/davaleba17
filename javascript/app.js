b2e1aaacf1810db1e70c10c215b5b676d52cfd94
GeorgeSaruashvili <giorgisaruashvili3@gmail.com>
2025-11-18 16:41:38 +0400

    iv

---
+++ javascript/app.js
@@ -0,0 +1,23 @@
+//I
+for (let i = 0; i < 25; i++) {
+  console.log(i);
+}
+//II
+let i = 0;
+while (i < 10) {
+  console.log(i);
+  i++;
+}
+//III
+let I = 0;
+do {
+  console.log(I);
+  i++;
+} while (I < 15);
+//IV
+const numbers = [23, 44, 123, 234, 765, 1023, 2368];
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] % 2 !== 0) {
+    console.log(numbers[i]);
+  }
+}
